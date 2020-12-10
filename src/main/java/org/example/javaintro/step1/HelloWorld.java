b934f6e36a40ce21ada446a63e004e406ce17708
Student <student@example.pl>
2020-12-10 11:46:27 +0100

    Step 1 - projekt maven, pierwsza klasa z metoda main i "Hello, World!"

---
+++ src/main/java/org/example/javaintro/step1/HelloWorld.java
@@ -0,0 +1,19 @@
+package org.example.javaintro.step1;
+/*
+    TODO 2 klasa - 'pudelko' z danymi i metodami
+    pelna nazwa to pakiet+nazwa klasy
+ */
+class HelloWorld {
+    // TODO 3
+    // metoda glowna - wejscie do programu javy
+    // uzywam skrotow intelij : psvm
+
+    public static void main(String[] args) {
+        // TODO 4
+        // wydukuj do konsoli napis : sout
+        System.out.println("Hello, World!");
+    }
+
+
+
+}
