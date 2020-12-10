06469632477acaf4e575dff4d6e7b3187bff2516
Student <student@example.pl>
2020-12-10 13:15:46 +0100

    Step 2 - klasa i jej składniki (pole i metoda)

--- src/main/java/org/example/javaintro/step1/HelloWorld.java
+++ src/main/java/org/example/javaintro/step1/HelloWorld.java
@@ -5,10 +5,3 @@ class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
-
-/*
-    TODO 1 utworz pakiet org.example.javaintro.step1.exercises
-    TODO 2 utworz dwolna klase z metoda main i w niej wydrukuj napis do konsoli
-    uzyj skrotow psvm, sout
-    TODO 3 commit zmian do GIT
- */
